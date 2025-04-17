28927f3a52503b2fe1bd9e81f5737e11704dd448
Naeim Badawi <Naeim.Badawi@Gmail.com>
2025-04-17 12:56:49 +0200

    (function move

--- XO_Game.py
+++ XO_Game.py
@@ -1,3 +1,7 @@
+"""
+الخطوة 1: دالة تقييم اللوحة
+(evaluate board)
+"""
 def evaluate(board):
     # التحقق من فوز X
     if 'xxx' in board:
@@ -11,3 +15,15 @@ def evaluate(board):
     # اللعبة لم تنته بعد
     else:
         return '-'
+    
+def move(board, mark, position):
+    if position < 0 or position >= len(board):
+        return board  # الموضع غير صالح، نعيد اللوحة كما هي
+    
+    # تحويل اللوحة إلى قائمة لتسهيل التعديل
+    board_list = list(board)
+    if board_list[position] == '-':
+        board_list[position] = mark
+    return ''.join(board_list)
+
+    
